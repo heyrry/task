79ef0c1003c3b7dd6da58d7183c151a110c3627e
heyan <baofeng.hy@alibaba-inc.com>
2025-05-09 11:16:29 +0800

    feat(task): 新增任务管理系统- 添加任务管理相关的实体类、枚举类、异常类和工具类 - 实现任务注册、调度、重试、删除等功能- 添加示例任务监听器- 配置数据库连接和Redis连接

--- src/main/java/com/herry/task/TaskApplication.java
+++ src/main/java/com/herry/task/TaskApplication.java
@@ -11,12 +11,7 @@ import java.sql.SQLException;
 public class TaskApplication {
 
 	public static void main(String[] args) throws Exception {
-		/*Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection conn = DriverManager.getConnection(
-				"jdbc:mysql://127.0.0.1:3306/task_demo?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
-				"root",
-				"heyan123456"
-		);*/
+
 		SpringApplication.run(TaskApplication.class, args);
 	}
 
